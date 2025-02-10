d461839ce67df976ebd2deed974cfd628015fdfe
Jima Benjamin Wani <99620235+jima05@users.noreply.github.com>
2025-02-10 07:37:05 +0300

    Update glide_systems.js

--- Lesson_37/glide_systems.js
+++ Lesson_37/glide_systems.js
@@ -6,5 +6,5 @@ gs.info("Current User Id: "+ user.getID());
 gs.info("Current User Roles : \n "+ user.getRoles());
 
 gs.info(" \n Current Instance : "+ gs.getProperty('instance_name'));
-gs.info("  Current url : "+ gs.getProperty('instance_url'));
+gs.info("  Current url : "+ gs.getProperty('glide.servlet.uri'));
 
