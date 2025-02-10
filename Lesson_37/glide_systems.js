d5bdd84ad6ded4fac8ecb8987cdd922a7c33ff01
Jima Benjamin Wani <99620235+jima05@users.noreply.github.com>
2025-02-10 07:35:28 +0300

    Create glide_systems.js

---
+++ Lesson_37/glide_systems.js
@@ -0,0 +1,10 @@
+gs.info("Current User Name: "+ gs.getUserName());
+
+var user = gs.getUser();
+
+gs.info("Current User Id: "+ user.getID());
+gs.info("Current User Roles : \n "+ user.getRoles());
+
+gs.info(" \n Current Instance : "+ gs.getProperty('instance_name'));
+gs.info("  Current url : "+ gs.getProperty('instance_url'));
+
